6e959900bcff26044d44d6b34040f0ef0fd70e24
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-27 10:11:27 -0400

    created config for font size

---
+++ atp-exphys/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontSize: {
+          '14pt': '14pt',
+          '16pt': '16pt',
+          '18pt': '18pt',
+          '20pt': '20pt',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
