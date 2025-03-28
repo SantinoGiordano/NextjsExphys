f596fc643bd6d20c0882659e6577676b42cb61b6
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-28 13:24:03 -0400

    created a footer

--- atp-exphys/components/Footer.tsx
+++ atp-exphys/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center text-white'>Created by santino giordano</div>
+    <div className='text-center p-5'>Created by santino giordano</div>
   )
 }
 
