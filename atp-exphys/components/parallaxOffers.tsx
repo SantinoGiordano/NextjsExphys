ee8cad1d528d2e048a68a13cc6462c3d55f36cc3
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-01 14:23:39 -0400

    parallax Footer section

--- atp-exphys/components/parallaxOffers.tsx
+++ atp-exphys/components/parallaxOffers.tsx
@@ -4,10 +4,9 @@ const ParallaxFooter = () => {
   return (
     <div
       className="h-[500px] bg-fixed bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('sunnyjog.jpg')" }}
+      style={{ backgroundImage: "url('mountainrange.jpg')" }}
     >
-      <div className="bg-black bg-opacity-50 p-6 text-white text-center rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold">Our Offers</h2>
+      <div className="bg-opacity-50 p-6 text-white text-center rounded-lg">
       </div>
     </div>
   );
