98f3982729e349a974673ca23431e97bcc24533c
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-02 11:55:57 -0400

    added reachUs id

--- atp-exphys/components/parallaxOffers.tsx
+++ atp-exphys/components/parallaxOffers.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const ParallaxFooter = () => {
   return (
     <div
+    id="ReachUs"
       className="h-[500px] bg-fixed bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: "url('mountainrange.jpg')" }}
     >
